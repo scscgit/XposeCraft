73989cb88723db95c9b4f81b74367b405f064c96
scsc <scscgit@users.noreply.github.com>
2016-11-05 18:38:25 +0100

    Creating a basic UI for the Menu.
    Created a UI>Canvas>Menu using UIController from Asset Store.
    Animated the Show/Hide events.
    Added non-default Arial font.
    Created a MenuController to wait for an Esc keypress and toggle Menu.
    The MenuController is added to an EventSystem in the Scene.

---
+++ Assets/Game/Scripts/UI/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+	public GameObject menu;
+	public bool MenuAtStart;
+
+	UIController ui;
+	Animator animator;
+
+	void Start()
+	{
+		this.ui = menu.GetComponent<UIController>();
+		this.animator = menu.GetComponent<Animator>();
+
+		if (!MenuAtStart)
+		{
+			// Pausing the animator, disabling the Menu
+			animator.enabled = false;
+			// Last started animation was Show, we explicitly set Hide
+			ui.Hide();
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Cancel"))
+		{
+			animator.enabled = true;
+			var show = ui.isShow;
+			Debug.logger.Log("Escape button pressed, " + (show ? "closing" : "opening") + " menu");
+			if (show)
+			{
+				ui.Hide();
+			}
+			else if (!show)
+			{
+				ui.Show();
+			}
+		}
+	}
+}
