172981b9f5ddf9c537c969119ce03f05308e0d28
scsc <scscgit@users.noreply.github.com>
2016-11-06 20:42:01 +0100

    First auto-creating of new units from a building. They collide.
    Added a debugging script that often creates units out of a base building.
    Created some spawn positions for the first building prefab.
    Renamed EightCrystals as their scale was 2.0 for a better relative size.
    Fixed Travis CI integration: used travis_wait to solve lack of log output.

---
+++ Assets/Game/Scripts/Debugging/AutomaticallyCreateUnits.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class AutomaticallyCreateUnits : MonoBehaviour
+{
+	public GameObject Unit;
+	public GameObject[] SpawnPositions;
+	public float CreationInterval = 20;
+
+	Transform unitsParent;
+	float timer = 0;
+
+	void Start()
+	{
+		this.unitsParent = GameObject.Find("Units").transform;
+	}
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer > CreationInterval)
+		{
+			timer -= CreationInterval;
+			CreateNewUnit();
+		}
+	}
+
+	bool CreateNewUnit()
+	{
+		if (SpawnPositions == null || SpawnPositions.Length == 0)
+		{
+			Debug.LogWarning(this + " does not have any position to spawn a unit");
+			return false;
+		}
+
+		foreach (var spawnPosition in SpawnPositions)
+		{
+			if (Spawn(spawnPosition) != null)
+			{
+				Debug.Log(this + " spawned " + Unit + " on position " + spawnPosition);
+				return true;
+			}
+		}
+		Debug.LogWarning(this + " could not spawn a unit on any position");
+		return false;
+	}
+
+	GameObject Spawn(GameObject spawnPosition)
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(spawnPosition.transform.position, Vector3.down, out hit))
+		{
+			return (GameObject)Instantiate(Unit, hit.point, Quaternion.FromToRotation(Unit.transform.up, hit.normal), unitsParent);
+		}
+		return null;
+	}
+}
